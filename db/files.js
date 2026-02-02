c502bab09f19322d5ca50113bf9ca22134309e50
JT <jstorbit@gmail.com>
2026-02-02 14:56:31 -0700

    Remove console logs

--- db/files.js
+++ db/files.js
@@ -2,9 +2,7 @@ import db from "./client.js";
 
 export const createFile = async ({name, size, folderId}) => {
   let newSize = null;
-  console.log("SIZE: ", size);
   size ? newSize = size : newSize = Math.floor(Math.random() * 301) + 1;
-  console .log("NEW SIZE: ", newSize)
   const sql = `
         INSERT INTO files (name, size, folder_id)
         VALUES ($1, $2, $3)
