0fa8213e9b57ec975fcd155685fcd55fa932608d
JT <jstorbit@gmail.com>
2026-02-02 14:47:35 -0700

    Update createFile function and seed to reflect object parameter, create files add post route

--- api/folders.js
+++ api/folders.js
@@ -3,6 +3,7 @@ const foldersRouter = express.Router()
 export default foldersRouter;
 
 import { getAllFolders, getFolder } from '#db/folders';
+import { createFile } from '#db/files';
 
 foldersRouter.use(express.json())
 
@@ -16,4 +17,16 @@ foldersRouter.get('/:id', async (req, res, next) => {
   const folderAndFiles = await getFolder(id);
   if (!folderAndFiles) return res.status(404).send(`Cannot find folder with id ${id}`)
   res.send(folderAndFiles);
+})
+
+foldersRouter.post('/:id/files', async (req, res, next) => {
+  const folderId = Number(req.params.id);
+  const foundFolder = await getFolder(folderId);
+  if (!foundFolder) return res.status(404).send(`Cannot find folder with id ${folderId}`);
+  if (!req.body) return res.status(400).send('Must include body in request');
+  const { name, size } = req.body;
+  if (!name || !size) return res.status(400).send('Must include name and size of file')
+  const fileToAdd = { name, size, folderId };
+  const newFile = await createFile(fileToAdd)
+  res.status(201).send(newFile);
 })
